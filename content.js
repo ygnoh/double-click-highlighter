e2f46953ab0d3f97847b54c7d2cbba07cd160b8c
yonggoonoh <nohyg90@gmail.com>
2020-11-11 00:18:30 +0900

    fix(#2): Fix color lost issue when esc pressed

--- content.js
+++ content.js
@@ -1,40 +1,59 @@
-const CLS = "__auto-highlight__";
+const CLS = "__double-click-highlighter__";
 
-chrome.runtime.onMessage.addListener(function (msg) {
+chrome.runtime.onMessage.addListener(msg => {
     if (msg.name !== "loaded") {
         return;
     }
 
-    document.addEventListener("dblclick", replace);
+    const spans = Array.from(document.getElementsByTagName("span"));
+
+    document.addEventListener("dblclick", highlight);
 
     document.addEventListener("keydown", e => {
         if (e.key !== "Escape") {
             return;
         }
 
-        replace();
+        highlight();
     });
 
-    function replace() {
+    function highlight() {
         const text = window.getSelection().toString();
+        const specialChar = /\W/.test(text);
 
-        if (/\W/.test(text)) {
+        if (specialChar) {
             return;
         }
 
+        restore();
+
+        if (!text.trim()) {
+            // esc
+            return;
+        }
+
+        color();
+
+        function color() {
+            spans.forEach(span => {
+                const {textContent} = span;
+
+                if (!new RegExp(`[^\\w\\s]*\\b${text}\\b[^\\w\\s]*`).test(textContent)) {
+                    return;
+                }
+
+                // todo what about using appendChild?
+                span.innerHTML = textContent.replaceAll(
+                    text,
+                    `<span class="${CLS}" style="background-color: yellow;">${text}</span>`
+                );
+            });
+        }
+    }
+
+    function restore() {
         Array.from(document.getElementsByClassName(CLS)).forEach(el => {
             el.outerHTML = el.textContent;
-        });
-
-        Array.from(document.getElementsByTagName("span")).forEach(el => {
-            if (!new RegExp(`[^\\w\\s]*\\b${text}\\b[^\\w\\s]*`).test(el.textContent)) {
-                return;
-            }
-
-            el.innerHTML = el.textContent.replaceAll(
-                text,
-                `<span class="${CLS}" style="background-color: yellow;">${text}</span>`
-            );
         });
     }
 });
